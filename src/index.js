dd6afe75c864c433820f6943b6fb593038ca6f60
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2024-08-04 19:48:15 +0300

    fix/need to use persistStore to not lose info on reload page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App.js';
-import store from './redux/store';
+
+// import store from './redux/store';
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,18 @@ import authReducer from '../store/auth/authReducer';
 import { thunk } from 'redux-thunk'; 
 import logger from 'redux-logger'; 
 
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist';
+
+
+// I used it to not lose information on reload page, because is memory saved info. 
+const persistConfig = {
+  key: 'root', // The key for the root reducer in storage
+  storage,     // Type of storage (localStorage in this case)
+  whitelist: ['auth'] // Specify which reducers you want to persist
+};
+
+
 // Combine reducers
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -12,10 +24,14 @@ const rootReducer = combineReducers({
   user: userReducers,
 });
 
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // Configure store
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
 });
 
+export const persistor = persistStore(store);
 export default store;
